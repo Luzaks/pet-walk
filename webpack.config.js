f816c26d0694dbb4a502b28cefe4016780466958
Luzaks <kanemekanik@gmail.com>
2020-08-30 20:50:04 -0500

    Initialize repository.

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const htmlPlugin = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: './index.html'
+});
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  plugins: [htmlPlugin]
+};

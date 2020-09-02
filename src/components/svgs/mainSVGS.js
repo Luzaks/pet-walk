770b06adce5b8535d4e1c61b805b5dbc841f943f
Luzaks <kanemekanik@gmail.com>
2020-09-02 13:45:01 -0500

    [Feat] Añade animación para el componente mainSVG.

--- src/components/svgs/mainSVGS.js
+++ src/components/svgs/mainSVGS.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../../styles/index.css';
 
 const Progress = ({ start, end }) => (
   <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
     <g filter="url(#filter0_i)">
       <circle cx="20" cy="20" r="15" stroke="#E5E6E8" strokeWidth="10" />
     </g>
-    <path d="M28.6251 7.72778C26.0992 5.95261 23.0873 5 20.0001 5" stroke="url(#paint0_angular)" strokeWidth="10" strokeLinecap="round" />
+    <path className="circle" x="20" y="20" r="15" strokeDasharray="20, 100" d="M18 2.0845 a 15.9155 15.9155 0 0 1 0 31.831 a 15.9155 15.9155 0 0 1 0 -31.831" stroke="url(#paint0_angular)" strokeWidth="10" strokeLinecap="round" />
     <defs>
       <filter id="filter0_i" x="0" y="0" width="44" height="44" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
         <feFlood floodOpacity="0" result="BackgroundImageFix" />
@@ -19,8 +20,8 @@ const Progress = ({ start, end }) => (
         <feBlend mode="normal" in2="shape" result="effect1_innerShadow" />
       </filter>
       <radialGradient id="paint0_angular" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(20 20) rotate(-176.186) scale(15.0333)">
-        <stop stopColor={start} />
-        <stop offset="1" stopColor={end} />
+        <stop stopColor="#87ABE1" />
+        <stop offset="1" stopColor="#CBE0FF" />
       </radialGradient>
     </defs>
   </svg>

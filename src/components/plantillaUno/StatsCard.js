72c90bdc3fe35490583cd6c7511bf793d6388411
Luzaks <kanemekanik@gmail.com>
2020-09-03 00:35:54 -0500

    [Fix] Corrige inset en el componente StatsCard.

--- src/components/plantillaUno/StatsCard.js
+++ src/components/plantillaUno/StatsCard.js
@@ -14,13 +14,21 @@ const StatsCard = ({ page }) => {
     title, description, color, endColor, progress, selected,
   } = page;
   const [filled, setFilled] = useState('');
+  const [inset, setInset] = useState('');
 
   useEffect(() => {
-    if (selected) setFilled('#1D6CE3');
-    else setFilled('#111212');
+    if (selected) {
+      setInset('inset 10px 10px 5px -3px rgba(0,0,0,0.05)');
+      setFilled('#1D6CE3');
+    } else {
+      setInset('inset 14px 16px 65px -13px rgb(1241, 243, 246)');
+      setFilled('#111212');
+    }
   }, []);
   return (
-    <Card>
+    <Card
+      inset={inset}
+    >
       <StatsDescription>
         <StatsTitle
           color={filled}
